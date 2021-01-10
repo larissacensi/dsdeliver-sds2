90c09282deda6abcfbc05134ea30717b5e5af92b
unknown <larissacensi3@gmail.com>
2021-01-10 16:23:06 -0300

    Fixed Netlify Problems

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,18 +1,20 @@
-import './styles.css';
-import { ReactComponent as YoutubeIcon } from './youtube.svg';
-import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import "./styles.css";
+
+import { ReactComponent as YouTubeIcon } from './youtube.svg';
 import { ReactComponent as InstagramIcon } from './instagram.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
 
 function Footer() {
     return (
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
-                    <YoutubeIcon />
+                <a href="https://www.youtube.com/c/DevSuperior/" target="_new">
+                    <YouTubeIcon />
                 </a>
-                <a href="https://www.linkedin.com/school/devsupeior/" target="_new"></a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
                     <LinkedinIcon />
+                </a>
                 <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
                     <InstagramIcon />
                 </a>
